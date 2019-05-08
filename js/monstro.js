db173639f1f451d66aeef22942ca6bb12eeb1603
DanielaLourenco <lourencodaniela.s@gmail.com>
2019-05-08 17:49:39 -0300

    Refazendo o mundo

---
+++ js/monstro.js
@@ -0,0 +1,79 @@
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(130, 130, "img/monstroE.gif", (screen.width / 2), (screen.height / 1.7), "image");
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function() {
+        this.canvas.width = screen.width;
+        this.canvas.height = screen.height;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function(e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function(e) {
+            myGameArea.key = false;
+        })
+    },
+    clear: function() {
+
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image") {
+            ctx.drawImage(this.image,
+                this.x,
+                this.y,
+                this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) {
+        myGamePiece.image.src = "img/monstroE.gif";
+        myGamePiece.speedX = -1;
+    }
+    if (myGameArea.key && myGameArea.key == 39) {
+        myGamePiece.image.src = "img/monstroD.gif";
+        myGamePiece.speedX = 1;
+    }
+    if (myGameArea.key && myGameArea.key == 38) {
+        myGamePiece.speedY = -1;
+    }
+    if (myGameArea.key && myGameArea.key == 40) {
+        myGamePiece.speedY = 1;
+    }
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
